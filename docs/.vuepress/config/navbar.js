b21cef5cdb1a5a20ce8cd3ecf1eae49ee90d3a3e
伶俜 <zhancjian@163.com>
2024-05-27 14:18:02 +0800

    新增旅游攻略-潮州

--- docs/.vuepress/config/navbar.js
+++ docs/.vuepress/config/navbar.js
@@ -9,6 +9,11 @@ const navbar = [
     text:'资源导航',
     icon:'material-symbols:web-asset',
     link:'/nav/',
+  },
+  {
+    text:'旅游攻略',
+    icon:'material-symbols:travel-luggage-and-bags-rounded',
+    link:'/travel/wjgs27tp/',
   }
 ];
 
--- docs/.vuepress/config/notes.js
+++ docs/.vuepress/config/notes.js
@@ -1,22 +1,22 @@
 import { definePlumeNotesConfig } from "vuepress-theme-plume";
 
 const notes = definePlumeNotesConfig({
-  // dir: "notes",
-  // link: "/",
-  // notes: [
-  //   {
-  //     dir: "article",
-  //     link: "/article/",
-  //     sidebar: [
-  //       {
-  //         text: "文章列表",
-  //         collapsed: false,
-  //         icon: "material-symbols:travel-luggage-and-bags-rounded",
-  //         items: [],
-  //       },
-  //     ],
-  //   },
-  // ],
+  dir: "notes",
+  link: "/",
+  notes: [
+    {
+      dir: "travel",
+      link: "/travel/",
+      sidebar: [
+        {
+          text: "旅游行程",
+          collapsed: false,
+          icon: "material-symbols:travel-luggage-and-bags-rounded",
+          items: ['潮州之旅'],
+        },
+      ],
+    },
+  ],
 });
 
 export default notes;
